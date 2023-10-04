02b2c13284da102af6eabd627878d7cac0d52c75
Jss-on <jessiondiwangan@gmail.com>
2023-10-04 09:42:18 +0800

    a

--- examples/inference.py
+++ examples/inference.py
@@ -32,7 +32,7 @@ if __name__ == "__main__":
     # Load the trained model
     usb_storage = USBStorage()
     # model_path = usb_storage.get_file_path("best_audio_classifier_v1.pkl")
-    model = joblib.load("example/best_audio_classifier_v1.pkl")
+    model = joblib.load("examples/best_audio_classifier_v1.pkl")
 
     # Audio settings
     duration = 10  # 10 seconds
