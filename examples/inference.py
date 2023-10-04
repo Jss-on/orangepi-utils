f10aa47083dbcd9b98d79a4078d1cb909cc01bbf
Jss-on <jessiondiwangan@gmail.com>
2023-10-04 13:13:59 +0800

    added led indicators

--- examples/inference.py
+++ examples/inference.py
@@ -8,7 +8,7 @@ import time
 import sys
 
 sys.path.append(".")
-from src import USBStorage
+from src import USBStorage, GPIO
 
 
 # Function to capture audio using native ALSA command
@@ -28,9 +28,32 @@ def extract_mfcc_features(audio_data, sample_rate=16000):
     return np.mean(mfcc_features, axis=0).reshape(1, -1)
 
 
+def led_indicator(pin="PA20", time_interval=1, max_blink=3):
+    gpio.pinMode(pin, "out")
+    # Blink the LED
+    count_blink = 0
+    while True:
+        # Turn the LED on
+        gpio.digitalWrite(pin, 1)
+        time.sleep(time_interval)  # Wait for 1 second
+
+        # Turn the LED off
+        gpio.digitalWrite(pin, 0)
+        time.sleep(time_interval)  # Wait for 1 second
+
+        if count_blink == max_blink:
+            count_blink = 0
+            break
+
+        count_blink += 1
+
+
 if __name__ == "__main__":
     # Load the trained model
     usb_storage = USBStorage()
+    gpio = GPIO()
+    pin = "PA20"
+    gpio.pinMode(pin, "out")
     # model_path = usb_storage.get_file_path("best_audio_classifier_v1.pkl")
     model = joblib.load("examples/best_audio_classifier_v1.pkl")
 
@@ -39,9 +62,10 @@ if __name__ == "__main__":
     samplerate = 16000  # 16 kHz
     filename = "temp_audio.wav"  # Temporary audio file name
 
-    count = 0
+    count 0
     while True:
         # Capture audio
+        gpio.digitalWrite(pin, 1)
         print("Capturing audio...")
         audio_data = capture_audio(duration, samplerate, filename)
 
@@ -49,6 +73,7 @@ if __name__ == "__main__":
         mfcc_features = extract_mfcc_features(audio_data, samplerate)
 
         # Make prediction
+
         predicted_label = model.predict(mfcc_features)[0]
 
         # Get confidence level
@@ -57,5 +82,9 @@ if __name__ == "__main__":
 
         print(f"Predicted Label: {predicted_label} with confidence {confidence:.2f}")
 
+        result = ",".join(str(count),predicted_label,str(confidence))
+        usb_storage.append("inference result.txt",result)
+
+        gpio.digitalWrite(pin, 0)
         # Wait before capturing the next audio
         time.sleep(1)  # wait for 1 second
---
+++ infer.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+export PYTHONPATH=/home/pi/.local/lib/python3.11/site-packages
+/usr/bin/python3 /home/pi/orangepi-utils/examples/inference.py
